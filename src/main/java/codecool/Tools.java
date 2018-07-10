8b52ae5f11a6bfbadd944d0259e1ce07221903a2
przemekNachel <przemekNachel@gmail.com>
2018-07-10 13:12:15 +0200

    Change tempSudoku

--- src/main/java/codecool/Tools.java
+++ src/main/java/codecool/Tools.java
@@ -2,24 +2,20 @@ package codecool;
 
 public class Tools {
 
-    static int[][] tempSudoku = new int[][] {
-        {1, 0, 0,   0, 0, 0,   0, 0, 0},
-        {0, 0, 0,   0, 0, 0,   0, 0, 0},
-        {0, 0, 0,   0, 0, 0,   0, 0, 0},
+    static String[][] tempSudoku = new String[][] {
+            {" ", " ", " ",   " ", " ", "6",   "4", " ", "9"},
+            {"4", " ", " ",   " ", "1", "9",   "5", "8", " "},
+            {"5", " ", " ",   " ", " ", " ",   " ", "1", "7"},
 
-        {0, 0, 0,   0, 0, 0,   0, 0, 0},
-        {0, 0, 0,   0, 0, 0,   0, 0, 0},
-        {0, 0, 0,   0, 0, 0,   0, 0, 0},
+            {" ", " ", " ",   " ", " ", " ",   " ", " ", " "},
+            {"3", " ", " ",   " ", " ", " ",   " ", "5", "8"},
+            {"9", " ", " ",   " ", " ", " ",   "7", " ", " "},
 
-        {0, 0, 0,   0, 0, 0,   0, 0, 0},
-        {0, 0, 0,   0, 0, 0,   0, 0, 0},
-        {0, 0, 0,   0, 0, 0,   0, 0, 9}};
+            {" ", "3", " ",   " ", "6", " ",   " ", "7", " "},
+            {" ", "5", " ",   "7", " ", " ",   "8", " ", " "},
+            {"1", "4", " ",   "3", " ", "5",   " ", " ", "6"}};
 
-    public static int[][] readSudokuFromFile(String filename) {
-        return new int[9][9];
-    }
-
-    public static void printSudoku(int[][] sudoku) {
+    public static void printSudoku(String[][] sudoku) {
         for(int i=0; i<tempSudoku.length; i++) {
             for(int j=0; j<tempSudoku[i].length; j++) {
                 System.out.print(tempSudoku[i][j] + " ");
