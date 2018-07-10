b8f9f0c227affd54d954235541679f7b866f1242
przemekNachel <przemekNachel@gmail.com>
2018-07-10 12:09:13 +0200

    Add printSudoku method without lines

--- src/main/java/codecool/Tools.java
+++ src/main/java/codecool/Tools.java
@@ -2,11 +2,27 @@ package codecool;
 
 public class Tools {
 
+    static int[][] tempSudoku = new int[][] {
+        {1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 9}};
+
     public static int[][] readSudokuFromFile(String filename) {
         return new int[9][9];
     }
 
     public static void printSudoku(int[][] sudoku) {
-
+        for(int i=0; i<tempSudoku.length; i++) {
+            for(int j=0; j<tempSudoku[i].length; j++) {
+                System.out.print(tempSudoku[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
