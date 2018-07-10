03ba84517cb0d5dd23be7b963e5c6db091ebf052
przemekNachel <przemekNachel@gmail.com>
2018-07-10 12:00:34 +0200

    Add Tools methods signatures

---
+++ src/main/java/codecool/Tools.java
@@ -0,0 +1,12 @@
+package codecool;
+
+public class Tools {
+
+    public static int[][] readSudokuFromFile(String filename) {
+        return new int[9][9];
+    }
+
+    public static void printSudoku(int[][] sudoku) {
+
+    }
+}
