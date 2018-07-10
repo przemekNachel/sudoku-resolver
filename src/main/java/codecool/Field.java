d89d927e9b90a5e269e299af0a73010a6e6501b7
creat73 <nikill1125@gmail.com>
2018-07-10 14:52:00 +0200

    Create Field class

---
+++ src/main/java/codecool/Field.java
@@ -0,0 +1,31 @@
+package codecool;
+
+public class Field {
+    private int value;
+    private int rowId;
+    private int columnId;
+    private int squareId;
+
+    public Field(int value, int rowId, int columnId, int squareId){
+        this.value = value;
+        this.rowId = rowId;
+        this.columnId = columnId;
+        this. squareId = squareId;
+    }
+
+    public int getRowId() {
+        return rowId;
+    }
+
+    public int getColumnId() {
+        return columnId;
+    }
+
+    public int getSquareId() {
+        return squareId;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
