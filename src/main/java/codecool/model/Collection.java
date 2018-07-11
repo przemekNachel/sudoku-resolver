08722189f5f45dac7e440ae67d935ae5283f6946
przemekNachel <przemekNachel@gmail.com>
2018-07-11 13:50:31 +0200

    Remove unnecessary methods

--- src/main/java/codecool/model/Collection.java
+++ src/main/java/codecool/model/Collection.java
@@ -25,32 +25,4 @@ public class Collection {
     public ArrayList<Field> getListOfFieldsInThisRow() {
         return listOfFieldsInThisRow;
     }
-
-    public boolean isLastEmpty() {
-        ArrayList<Field> zeros = new ArrayList<>();
-        for (Field field : listOfFieldsInThisRow) if (field.getValue() == 0) zeros.add(field);
-        if (zeros.size() == 1) {
-            lastEmpty = zeros.get(0);
-            fillLastEmpty();
-            return true;
-        }
-        return false;
-    }
-
-    private void fillLastEmpty() {
-        ArrayList<Integer> values = new ArrayList<>();
-        for (Field field : listOfFieldsInThisRow) values.add(field.getValue());
-        for (int i = 1; i < 10; i ++) {
-            if (!values.contains(i)) lastEmpty.setValue(i);
-        }
-    }
-
-    private Field getFieldByValue(int value) {
-        for (Field field : listOfFieldsInThisRow) if (field.getValue() == value) return field;
-        return null;
-    }
-
-    public Field getLastEmpty() {
-        return lastEmpty;
-    }
 }
