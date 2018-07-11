1016aa45e88d837a915b51f63477e92e9d49b7b0
adamlup <adamlupi@gmail.com>
2018-07-11 10:01:47 +0200

    add algorithm class

--- src/main/java/codecool/Field.java
+++ src/main/java/codecool/Field.java
@@ -1,16 +1,24 @@
 package codecool;
 
+import java.util.ArrayList;
+
 public class Field {
     private int value;
     private int rowId;
     private int columnId;
     private int squareId;
+    private int emptyFieldValue = 0;
+    private ArrayList<Integer> probablyValues;
+    private int probablyListCapacity = 9;
 
     public Field(int value, int rowId, int columnId, int squareId){
         this.value = value;
         this.rowId = rowId;
         this.columnId = columnId;
         this.squareId = squareId;
+        if(value==emptyFieldValue){
+            probablyValues = new ArrayList<>(probablyListCapacity);
+        }
     }
 
     public int getRowId() {
@@ -31,5 +39,13 @@ public class Field {
 
     public void setValue(int value) {
         this.value = value;
+    }
+
+    public ArrayList<Integer> getProbablyValues() {
+        return probablyValues;
+    }
+
+    public void addProbablyValue(int value){
+        
     }
 }
