32d52d15cf2cf9eb19fb79e15fd439e2e186b367
adamlup <adamlupi@gmail.com>
2018-07-12 15:05:30 +0200

    add startTime variable to count time

--- src/main/java/codecool/App.java
+++ src/main/java/codecool/App.java
@@ -4,6 +4,7 @@ public class App {
 
     public static void main(String[] args) {
         int[][] sudoku = Tools.stringToArray(Tools.tempStringSudoku);
+        Resolver.startTime = System.currentTimeMillis();
         new Resolver(sudoku).run();
     }
 }
